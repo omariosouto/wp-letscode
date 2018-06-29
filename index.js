492561f040b78ab9eaaf6c112fbc4dcb8c0bf321
Mario Souto <soutomarios@gmail.com>
2018-06-29 03:34:34 -0300

    first steps working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+require('isomorphic-fetch');
 const program = require('commander')
 const { spawn } = require('child_process');
 const packageJsonInfo = require('./package.json')
@@ -30,14 +31,50 @@ program
         runDocker.stdout.on('data', (data) => runDockerSuccess += data);
         runDocker.stderr.on('data', (data) => runDockerError += data);
 
-        runDocker.on('close', (code) => {
-            if(runDockerSuccess.length) {
-                console.log('Project up and running :)')
+        runDocker.on('close', async (code) => {
+            if(runDockerError.includes(`Can't find a suitable configuration file in this directory`)) {
+                console.log(`[${global.name}] Make sure that you are in a directory that has an "docker-compose.yml" file`)
             }
-            if(runDockerError.length) {
-                console.log('Some problem occurred :(')
+            if(runDockerError.includes('up-to-date')) {
+                console.log(`[${global.name}] All Docker Containers for this project are already created and running...`)
+                console.log('Access: http://localhost:8080')
             }
+            if(runDockerError.includes('Creating')) {
+                console.log(`[${global.name}] All Docker Containers are starting to run...`)
+                recursiveFetch('http://localhost:8080/')
+                    .then(async (data) => { 
+                        const pageHtml = await data.text()
+
+                        if(pageHtml.includes('</html>')) {
+                            console.log('Your project is ready! Access: http://localhost:8080')
+                        }
+                    })
+                    .catch((error) => {
+                        console.log('First Error', error)
+                    });
+            }
+
         });
+
+        runDocker.on('error', (err) => {
+            console.log(`[${global.name}] ${err}. Please, open an issue at: https://github.com/omariosouto/wp-letscode/pulls`)
+        })
     })
+  
+const recursiveFetch = (url) => {
+    return fetch(url)
+    .then(data => { 
+        if(data.text) {
+            return data
+        } else {
+            return recursiveFetch(url)
+        }
+    })
+    .catch(error => {
+        return recursiveFetch(url)
+        // console.log('First Error', error)
+    });
+
+}
 
 program.parse(process.argv)
